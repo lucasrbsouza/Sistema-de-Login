a39d051a0981ef119d91278d47f733af5eb64ba2
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2025-03-24 10:32:44 -0300

    Primeiro commit

---
+++ src/main/java/io/github/lucasrbsouza/sistema_de_login/SistemaDeLoginApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasrbsouza.sistema_de_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/lucasrbsouza/sistema_de_login/SistemaDeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasrbsouza.sistema_de_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
